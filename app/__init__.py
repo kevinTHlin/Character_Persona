4315f989dc0c2ece7e5bdc514c85871d270c374c
kevinTHlin <tzhalin@sas.upenn.edu>
2021-08-02 18:39:24 +0800

    1st_ed

---
+++ app/__init__.py
@@ -0,0 +1,91 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import scipy
+from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
+from flask import Flask, request, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/table', methods=['GET', 'POST'])
+def character_table(e):
+    if request.method == 'POST':
+        class_start_date = request.form.get('date')
+        df_uploaded = pd.read_csv(request.files.get('file'))
+
+        class_start_date =  datetime.strptime(class_start_date , '%Y-%m-%d').date()
+        user = df_uploaded['get_user_ID'].unique()
+        
+        Table = {}     
+        Learners = {}
+        
+        for i in user:
+            Table[i] = df_uploaded.loc[df_uploaded['get_user_ID'] == i]
+            Table[i]['get_user_ID'] = Table[i]['get_user_ID'].astype(str)
+            Table[i] = Table[i].replace('不足1s', '00:00:01')
+            Table[i]['time_spent'] = pd.to_timedelta(Table[i].time_spent)
+            Table[i]['visit_time'] = pd.to_datetime(Table[i].visit_time, format='%Y.%m.%d %H:%M')
+            Table[i]['visit_date'] = Table[i]['visit_time'].dt.date
+            Table[i] = Table[i].sort_values('visit_time')
+            Table[i]['time_spent'] = Table[i]['time_spent'].dt.total_seconds()
+            Table[i] = Table[i].groupby(['visit_date']).agg(time_sum = 
+                                                            ('time_spent', 'sum'),
+                                                            visits_count = 
+                                                            ('visit_time', 'count')).reset_index()
+            Table[i]['day_difference'] = Table[i].diff(periods=1, axis=0)['visit_date'].fillna(pd.Timedelta(days=0))  
+            Table[i]['day_difference'] = Table[i]['day_difference'].apply(lambda x: x.days)
+            Table[i]['time_spent_pv'] = Table[i]['time_sum']/Table[i]['visits_count']        
+            Learners["LEARNER{0}".format(i)] = []
+        
+            sum_Q1 = Table[i]['time_sum'].quantile(0.25)
+            sum_Q3 = Table[i]['time_sum'].quantile(0.75)
+            sum_IQR = sum_Q3 - sum_Q1
+            
+            before_class = Table[i][(Table[i]['visit_date'] < class_start_date)]['time_sum']
+            after_class = Table[i][(Table[i]['visit_date'] >= class_start_date)]['time_sum']
+            if before_class.sum(axis=0) > 0:
+                meta = 1
+                moti = before_class.median()
+            else: 
+                meta = 0
+                moti = 0
+                        
+            half = round(Table[i].shape[0]/2)
+            first_half = Table[i].loc[:half, 'time_sum'] 
+            second_half = Table[i].loc[half:, 'time_sum']  
+            
+            #Six Character Features
+            Grit = Table[i]['time_sum'].median() / sum_IQR
+            Self_control = Table[i]['time_spent_pv'].median()
+            Meta_cog_Self_reg = (meta + (Table[i]['day_difference'].isin(Table[i]['day_difference'].mode()).count()/(Table[i]['day_difference'].nunique()))) / sum_IQR
+            Motivation = moti + after_class.median()
+            Engagement = Table[i]['time_sum'].median()
+            Self_perception = -math.log(1 + abs(first_half.median() - second_half.median()))
+                    
+            Learners["LEARNER{0}".format(i)].extend([str(i), Grit, Self_control, Meta_cog_Self_reg, Motivation, Engagement, Self_perception])    
+            
+        Learners_col = ['user_ID', 'Grit', 'Self_control', 'Meta_cog_Self_reg', 'Motivation', 'Engagement', 'Self_perception']
+        df_Learners = pd.DataFrame(columns = Learners_col)
+        for i in user:
+            a_series = pd.Series(Learners['LEARNER' + str(i)], index = df_Learners.columns)
+            df_Learners = df_Learners.append(a_series, ignore_index=True)
+        df_Learners.set_index('user_ID', inplace=True)
+        df_Learners = df_Learners.astype(np.float64)
+            
+        print(df_Learners.isnull().sum().sum())
+        print(df_Learners.isin([np.inf, -np.inf]).sum().sum())        
+        scaler = MinMaxScaler()    
+        df_Learners_scaled = df_Learners[:]
+        df_Learners_scaled = scaler.fit_transform(df_Learners_scaled)    
+        table = pd.DataFrame(df_Learners_scaled, index = df_Learners.index, 
+                                            columns = df_Learners.columns)
+        return render_template('index.html', table = table.to_html(classes='data'), title=table.columns.values)
+        
